ddf65e9fee5393c82fb4aa007e60135847ecd01f
Jezzarium <justinabourjaili@hotmail.com>
2025-12-30 16:04:18 -0500

    feat: Implement animated intro sequence with new Home component

---
+++ src/lib/Home.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { animate } from "animejs";
+
+    let home: HTMLElement;
+
+    onMount(() => {
+        animate(home, {
+            opacity: [0, 1],
+            filter: ["blur(10px)", "blur(0px)"],
+            duration: 800,
+            easing: "easeOutExpo",
+        });
+    });
+</script>
+
+<div class="home" bind:this={home}>
+    <h1 class="google-sans">Home cinsehtcsintahcsi</h1>
+</div>
+
+<style>
+    .home {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        color: white;
+        opacity: 0;
+    }
+</style>
--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import "./Main.css";
     let showSplat = $state(false);
+    let showHome = $state(false);
     import Splat from "./Splat.svelte";
     import Intro from "./Intro.svelte";
+    import Home from "./Home.svelte";
 </script>
 
 <main class="main">
@@ -17,5 +19,9 @@
             {showSplat ? "HIDE SPLAT" : "SHOW SPLAT"}
         </button>
     </div>
-    <Intro />
+    {#if showHome}
+        <Home />
+    {:else}
+        <Intro oncomplete={() => (showHome = true)} />
+    {/if}
 </main>
